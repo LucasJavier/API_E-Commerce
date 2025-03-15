af018ce0a58b635ae8aff390853a819ba436c8bb
LucasJavier <lucasbonacci@hotmail.com>
2025-03-14 21:40:18 -0300

    CRUD de nueva entidad 'Category'

---
+++ src/category/category.controller.ts
@@ -0,0 +1,56 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Categories') // Agrupa en Swagger
+@Controller('category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a category' })
+  @ApiResponse({ status: 201, description: 'Category successfully created' })
+  @ApiResponse({ status: 500, description: 'Error creating category' })
+  @ApiBody({ type: CreateCategoryDto })
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all categories' })
+  @ApiResponse({ status: 200, description: 'Lista de categorías' })
+  @ApiResponse({ status: 404, description: 'No categories found' })
+  findAll() {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Obtain a category' })
+  @ApiParam({ name: 'id', description: 'Category ID', example: 1 })
+  @ApiResponse({ status: 200, description: 'Category found' })
+  @ApiResponse({ status: 404, description: 'Category not found' })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update a category' })
+  @ApiParam({ name: 'id', description: 'ID of the category to be updated', example: 1 })
+  @ApiBody({ type: UpdateCategoryDto })
+  @ApiResponse({ status: 200, description: 'Category correctly updated' })
+  @ApiResponse({ status: 404, description: 'Category not found' })
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return this.categoryService.update(id, updateCategoryDto);
+  }
+
+  @Delete(':id')  
+  @ApiOperation({ summary: 'Delete a category' })
+  @ApiParam({ name: 'id', description: 'ID of the category to be deleted', example: 1 })
+  @ApiResponse({ status: 200, description: 'Category correctly eliminated' })
+  @ApiResponse({ status: 404, description: 'Category not found' })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.categoryService.remove(id);
+  }
+}
