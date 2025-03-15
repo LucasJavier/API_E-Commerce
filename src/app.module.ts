64d1cb1bab052c44bf6c708e00cdf61d4f7895a2
LucasJavier <lucasbonacci@hotmail.com>
2025-03-15 13:58:28 -0300

    Agregue el modulo de Product a los import de la app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,10 @@ import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductModule],
+  imports: [ConfigModule.forRoot(), ProductModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule.forRoot(),CategoryModule],
 })
 export class AppModule {}
