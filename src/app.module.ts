f0fce02a31a14f3bca07f475acfbcefc9486adbd
Jose <joseantille@gmail.com>
2025-03-19 17:40:18 -0300

    Actualizando archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoryModule } from './category/category.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { ItemsModule } from './items/items.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+<<<<<<< HEAD
 import { ProductModule } from './product/product.module';
 import { WishlistModule } from './wishlist/wishlist.module';
 
@@ -10,5 +13,24 @@ import { WishlistModule } from './wishlist/wishlist.module';
   imports: [ConfigModule.forRoot(), ProductModule, CategoryModule, WishlistModule],
   controllers: [AppController],
   providers: [AppService],
+=======
+import { CognitoAuthController } from './cognito-auth/cognitoAuth.controller';
+import { CognitoAuthModule } from './cognito-auth/cognitoAuth.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    //ProductsModule,
+    //ItemsModule,
+    //UserModule,
+    //AuthModule,
+    CognitoAuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+>>>>>>> SCRUM-4-6_Cognito-Roles_Usuarios
 })
 export class AppModule {}
