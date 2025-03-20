6dbb0a88b1ffaaf6838d0ad472c9d08fa3b10721
Jose <joseantille@gmail.com>
2025-03-20 17:42:11 -0300

    Creacion de DTO CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
 
-    
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    userId: string;
 
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    role: string;
+    
 }
+  
