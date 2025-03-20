a54c3b3d3fb320caaa397bd0fd9b509bf65acec4
LucasJavier <lucasbonacci@hotmail.com>
2025-03-20 10:14:07 -0300

    Agregando guard de roles a las rutas

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/guard-roles/guard-roles.guard';
+import { AcceptedRoles } from 'src/guard-roles/role.decorator';
 
-@ApiTags('Categories') // Agrupa en Swagger
+@ApiTags('Categories') // Group in Swagger
 @Controller('category')
+@UseGuards(RolesGuard)
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @AcceptedRoles('Admin') // Only Admin users can create categories
   @ApiOperation({ summary: 'Create a category' })
   @ApiResponse({ status: 201, description: 'Category successfully created' })
   @ApiResponse({ status: 500, description: 'Error creating category' })
@@ -19,6 +23,7 @@ export class CategoryController {
   }
 
   @Get()
+  @AcceptedRoles('Admin', 'User') // All users can see the categories
   @ApiOperation({ summary: 'Get all categories' })
   @ApiResponse({ status: 200, description: 'Lista de categorías' })
   @ApiResponse({ status: 404, description: 'No categories found' })
@@ -27,6 +32,7 @@ export class CategoryController {
   }
 
   @Get(':id')
+  @AcceptedRoles('Admin', 'User') // All users can see a category
   @ApiOperation({ summary: 'Obtain a category' })
   @ApiParam({ name: 'id', description: 'Category ID', example: 1 })
   @ApiResponse({ status: 200, description: 'Category found' })
@@ -36,6 +42,7 @@ export class CategoryController {
   }
 
   @Patch(':id')
+  @AcceptedRoles('Admin') // Only Admin users can update categories
   @ApiOperation({ summary: 'Update a category' })
   @ApiParam({ name: 'id', description: 'ID of the category to be updated', example: 1 })
   @ApiBody({ type: UpdateCategoryDto })
@@ -45,7 +52,8 @@ export class CategoryController {
     return this.categoryService.update(id, updateCategoryDto);
   }
 
-  @Delete(':id')  
+  @Delete(':id')
+  @AcceptedRoles('Admin') // Only Admin users can delete categories  
   @ApiOperation({ summary: 'Delete a category' })
   @ApiParam({ name: 'id', description: 'ID of the category to be deleted', example: 1 })
   @ApiResponse({ status: 200, description: 'Category correctly eliminated' })
