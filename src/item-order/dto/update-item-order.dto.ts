4ec29cd35db5a34fc7a58d45c1aa7f6a9e207389
LucasJavier <lucasbonacci@hotmail.com>
2025-04-11 12:16:07 -0300

    elimnar .spec e item-order

--- src/item-order/dto/update-item-order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateItemOrderDto } from './create-item-order.dto';
-
-export class UpdateItemOrderDto extends PartialType(CreateItemOrderDto) {}
