b0284d616533098fddda6d47a3828fb08a3c65d0
Jose <joseantille@gmail.com>
2025-04-10 21:19:03 -0300

    Pruebas unitarias de items del carrito y eliminacion de pruebas de controladores

--- src/guard-roles/guard-roles.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GuardRolesGuard } from './guard-roles.guard';
-
-describe('GuardRolesGuard', () => {
-  it('should be defined', () => {
-    expect(new GuardRolesGuard()).toBeDefined();
-  });
-});
--- src/item-order/item-order.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ItemOrderService } from './item-order.service';
-import { ItemOrderController } from './item-order.controller';
-
-@Module({
-  controllers: [ItemOrderController],
-  providers: [ItemOrderService],
-})
-export class ItemOrderModule {}
